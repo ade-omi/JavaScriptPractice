6a28aef3602297635740fe218b9a6dfd93aff8a3
ade-omi <76014664+ade-omi@users.noreply.github.com>
2021-10-04 14:11:46 -0400

    Small program practicing flow control

---
+++ MagicEightBall.js
@@ -0,0 +1,35 @@
+//A small program using flow control in JavaScript, 
+let userName = 'dapo';
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+let userQuestion = "Whats the case of flight in september";
+console.log(`${userName} asked: ${userQuestion}`);
+
+const randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+switch (randomNumber) {
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1:
+        eightBall = 'It is decidedly so';
+        break;
+    case 2:
+        eightBall = 'Reply hazy try again';
+        break;
+    case 3:
+        eightBall = 'Cannot predict now';
+        break;
+    case 4:
+        eightBall = 'Do not count on it';
+        break;
+    case 5:
+        eightBall = 'My sources say no';
+        break;
+    case 6:
+        eightBall = 'Outlook not so good';
+        break;
+    case 7:
+        eightBall = 'Signs point to yes';
+        break;
+}
+console.log(`The eight ball answered: ${eightBall}`);
